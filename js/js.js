e152dcd293c8a292c8ee5e3f9e2d6effda3b979a
Rakib_Penta <rakib@pentabd.com>
2020-04-21 13:23:58 +0600

    Lighting and aspect ratio

--- js/js.js
+++ js/js.js
@@ -1,25 +1,66 @@
 const canvas = document.getElementById('particularCanv')
 const renderer = new THREE.WebGLRenderer({canvas})
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 
 const fov = 75;
-const aspect = window.innerWidth / window.innerHeight;  // the canvas default
+let aspect = window.innerWidth / window.innerHeight;  // the canvas default
 const near = 0.1;
 const far = 500;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 5;
+
+
+window.addEventListener('resize', () => {
+    renderer.setSize( window.innerWidth, window.innerHeight );
+})
 
 
 const scene = new THREE.Scene()
 
 
 const geometry = new THREE.BoxGeometry();
-const metarial = new THREE.MeshBasicMaterial({color: 0x44aa88});
 
 
-const cube = new THREE.Mesh(geometry, metarial)
+const newCube = (color, xPosition) => {
+    const metarial = new THREE.MeshPhongMaterial({color: color});
+    const cube = new THREE.Mesh(geometry, metarial)
+    cube.position.x = xPosition
+    
+    return cube
+}
+
+const cubes = [newCube('red', 5), newCube('yellow', 0), newCube('aqua', -5)]
+
+cubes.forEach(item => {
+    scene.add(item);
+    scene.add(item);
+    scene.add(item);
+    animateCube(item)
+})
 
 
-scene.add(cube);
 
-renderer.render(scene, camera);
+const lightColor = 0xFFFFFF
+const intensity = 1
+const light = new THREE.DirectionalLight(lightColor, intensity)
+light.position.set(-1, 2, 10)
+
+scene.add(light)
+
+
+function animateCube (cube) {
+    const render = () => {
+        cube.rotation.x += .02
+        cube.rotation.y += .02
+
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+
+
+        renderer.render(scene, camera);
+        requestAnimationFrame(render)
+    }
+    requestAnimationFrame(render)
+}
