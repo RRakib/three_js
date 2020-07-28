757052f0bb01b501da7773db8c548a5b45618200
Rakib_Penta <16103030@iubat.edu>
2020-07-28 22:38:59 +0600

    Solar system initialized

--- js/solarsystem.js
+++ js/solarsystem.js
@@ -8,12 +8,14 @@ const fov = 40;
 const aspect = window.innerWidth/window.innerHeight;
 const near = .1;
 const far = 1000;
-const camera = new THREE.Camera(fov, aspect, near, far);
-camera.position.z = 120; 
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.set(0, 50, 0);
+camera.up.set(0, 0, 1);
+camera.lookAt(0, 0, 0);
+
 
 // Scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color('#ffffff');
 
 const objects = [];
 
@@ -29,8 +31,14 @@ scene.add(sunMesh);
 objects.push(sunMesh)
 
 
-function startLoop(){
-    renderer.render(scene, camera)
-}
+function startLoop(time){
+    time *= 0.001;
 
+    objects.forEach((obj) => {
+        obj.rotation.y = time;
+      });
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(startLoop)
+}
 requestAnimationFrame(startLoop)
